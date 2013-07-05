64e549f95921f6d2feaa93b927ac0ba059a6b756
Jeff Dyer <jeffdyer@acm.org>
2013-07-04 21:23:58 -0700

    Fix endian-ness; implement start/finishString

--- src/builder.js
+++ src/builder.js
@@ -34,9 +34,9 @@
      writeF32(val)
      writeF64(val)
 
-     Foreign function interface
+     imported functions
 
-     ffi = {
+     imports = {
        resizeHeap,
      }
 
@@ -70,13 +70,13 @@ var trace = function trace(str) {
 
 var Builder = (function () {
 
-  var Builder = function Builder(stdlib, ffi, heap) {
+  var Builder = function Builder(stdlib, imports, heap) {
     "use asm";
 
     var pos = 0;
     var bytes = new stdlib.Uint8Array(heap);
 
-    var trace = ffi.trace;
+    var trace = imports.trace;
 
     var $_ = 95;   // place holder
     var $o = 111;
@@ -85,6 +85,8 @@ var Builder = (function () {
     var $i = 105;
     var $I = 73;
     var $L = 76;
+    var $s = 115;
+    var $S = 83;
 
     function startObject(isBig) {
       isBig = isBig | 0;
@@ -109,11 +111,47 @@ var Builder = (function () {
       if ((isBig | 0) == 0) {
         bytes[offset + 1 | 0] = count;
       } else {
-        bytes[(offset + 1) | 0] = count & 0xFF;
-        bytes[(offset + 2) | 0] = count >> 8 & 0xFF;
-        bytes[(offset + 3) | 0] = count >> 16 & 0xFF;
-        bytes[(offset + 4) | 0] = count >> 24 & 0xFF;
+        bytes[(offset + 1) | 0] = count >> 24 & 0xFF;
+        bytes[(offset + 2) | 0] = count >> 16 & 0xFF;
+        bytes[(offset + 3) | 0] = count >> 8 & 0xFF;
+        bytes[(offset + 4) | 0] = count & 0xFF;
       }
+    }
+
+    function startString(isBig) {
+      isBig = isBig | 0;
+      if ((isBig | 0) == 0) {
+        bytes[pos] = $s;
+        bytes[pos + 1 | 0] = $_;
+        pos = pos + 2 | 0;
+      } else {
+        bytes[pos] = $S;
+        bytes[pos + 1 | 0] = $_;
+        bytes[pos + 2 | 0] = $_;
+        bytes[pos + 3 | 0] = $_;
+        bytes[pos + 4 | 0] = $_;
+        pos = pos + 5 | 0;
+      }
+    }
+
+    function finishString(offset, count, isBig) {
+      offset = offset | 0;
+      count = count | 0;
+      isBig = isBig | 0;
+      if ((isBig | 0) == 0) {
+        bytes[offset + 1 | 0] = count;
+      } else {
+        bytes[(offset + 1) | 0] = count >> 24 & 0xFF;
+        bytes[(offset + 2) | 0] = count >> 16 & 0xFF;
+        bytes[(offset + 3) | 0] = count >> 8 & 0xFF;
+        bytes[(offset + 4) | 0] = count & 0xFF;
+      }
+    }
+
+    function writeChar(val) {
+      val = val | 0; 
+      bytes[pos] = val;
+      pos = pos + 1 | 0;
     }
 
     function writeByte(val) {
@@ -126,9 +164,19 @@ var Builder = (function () {
     function writeI16(val) {
       val = val | 0;
       bytes[pos] = $i;
-      bytes[pos + 1 | 0] = val & 0xFF;
-      bytes[pos + 2 | 0] = val >> 8 & 0xFF;
+      bytes[pos + 1 | 0] = val >> 8 & 0xFF;
+      bytes[pos + 2 | 0] = val & 0xFF;
       pos = pos + 3 | 0;
+    }
+
+    function writeI32(val) {
+      val = val | 0;
+      bytes[pos] = $I;
+      bytes[pos + 1 | 0] = val >> 24 & 0xFF;
+      bytes[pos + 2 | 0] = val >> 16 & 0xFF;
+      bytes[pos + 3 | 0] = val >> 8 & 0xFF;
+      bytes[pos + 4 | 0] = val & 0xFF;
+      pos = pos + 5 | 0;
     }
 
     function position() {
@@ -136,13 +184,16 @@ var Builder = (function () {
     }
 
     return {
+      writeChar: writeChar,
       writeByte: writeByte,
       writeI16: writeI16,
+      writeI32: writeI32,
       startObject: startObject,
       finishObject: finishObject,
+      startString: startString,
+      finishString: finishString,
       position: position,
     };
-
   }
 
   // Self test
@@ -154,16 +205,24 @@ var Builder = (function () {
     }
     var builder = Builder(global, imports, buffer);
     var view = new Uint8Array(buffer);
-    var pos = builder.position();
+    var pos = [];
+    pos.push(builder.position());
     builder.startObject();
     builder.writeByte(10);
-    builder.writeI16(100);
-    builder.finishObject(pos, 1);
+    pos.push(builder.position());
+    builder.startString();
+    builder.writeChar("h".charCodeAt(0));
+    builder.writeChar("e".charCodeAt(0));
+    builder.writeChar("l".charCodeAt(0));
+    builder.writeChar("l".charCodeAt(0));
+    builder.writeChar("o".charCodeAt(0));
+    builder.finishString(pos.pop(), 5);
+    builder.finishObject(pos.pop(), 1);
     dumpView();
 
     function dumpView() {
       var i = 0;
-      for (; i < 10; i = i + 1) {
+      for (; i < 20; i = i + 1) {
         trace(view[i]);
       }
     }
